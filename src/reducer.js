ee79db5ad3ab18506f127586f424d5b254f0095e
isacc-co <izaqfirew@gmail.com>
2021-05-24 05:59:11 -0700

    firstcommit

---
+++ src/reducer.js
@@ -0,0 +1,76 @@
+import CartItem from "./CartItem";
+
+const reducer = (state,action)=>{
+    if(action.type === 'CLEAR_CART'){
+        return{...state,cart:[]}
+    }
+    if(action.type === 'REMOVE'){
+        return {
+            ...state, 
+            cart:state.cart.filter((CartItem)=>CartItem.id !== action.payload),
+        }
+    }
+    if(action.type === 'INCREASE'){
+        let tempCart=state.cart.map((CartItem)=>{
+            if(CartItem.id === action.payload){
+     return {...CartItem,amount:CartItem.amount + 1}
+            }
+            return CartItem
+
+        });
+        return {...state,cart:tempCart}
+    }
+
+
+    if(action.type === 'DECREASE'){
+        let tempCart=state.cart.map((CartItem)=>{
+            if(CartItem.id === action.payload){
+     return {...CartItem,amount:CartItem.amount - 1}
+            }
+            return CartItem
+
+        }).filter((CartItem)=>CartItem.amount !== 0)
+        return {...state,cart:tempCart}
+    }
+if(action.type === 'GET_TOTALS'){
+    let {total,amount } = state.cart.reduce((cartTotal,CartItem)=>{
+        const{price,amount} = CartItem
+        const itemTotal = price*amount
+        cartTotal.total += itemTotal
+        cartTotal.amount += amount
+        return cartTotal
+    },{
+        total:0,
+        amount:0
+    }
+    )
+    total = parseFloat(total.toFixed(2))
+
+    return{...state,total,amount}
+}
+if(action.type === 'LOADING'){
+    return {...state,loading:true}
+}
+if(action.type === 'DISPLAY_ITEM'){
+    return{...state, cart:action.payload, loading:false}
+}
+
+if(action.type === 'TOGGLE_AMOUNT'){
+    let tempCart = state.cart.map((CartItem)=>{
+if(CartItem.id === action.payload.id){
+if(action.payload.type === 'inc'){
+return {...CartItem,amount:CartItem.amount + 1}
+}
+if(action.payload.type === 'dec'){
+    return {...CartItem,amount:CartItem.amount - 1}
+}
+}
+return CartItem
+    })
+    .filter((CartItem)=>CartItem.amount !== 0)
+    return {...state,cart:tempCart}
+}
+    throw new Error('no matching action type')
+}
+
+export default reducer;
